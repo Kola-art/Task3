ed72d981bb6f24c5c72d33a031039ed52333b2aa
Artem Kolodka <73796530+Kola-art@users.noreply.github.com>
2021-03-19 17:26:57 +0200

    task3 addition

--- src/App.js
+++ src/App.js
@@ -1,36 +1,38 @@
 import React from 'react';
 import './App.css';
-
+import { Enteredtext } from './components/inputtext';
 
 class App extends React.Component  {
-    constructor(props){
-      super(props);
-      this.state = {
-        userName: 'гость',
-      }
-     
+  constructor(props){
+    super(props);
+    this.state = {
+      userName: 'гость',
     }
- 
-    greetingText(){
-      return(
-        <p>Привет, { this.state.userName }  !  </p>
-      )
+   
+  }
+
+  greetingText(){
+    return(
+      <p>Привет, { this.state.userName }  !  </p>
+    )
+  }
+  handleChange(event){
+    if(event.target.value === ""){
+      this.setState({userName : 'гость'});
     }
-    handleChange(event){
-      if(event.target.value === ""){
-        this.setState({userName : 'гость'});
-      }
-      else this.setState({userName : event.target.value});
-    }
+    else this.setState({userName : event.target.value});
+  }
+   
   render(){
+   
     return (
-     
+          <div className="App">
+              <Enteredtext />
           <div className="App">
             {this.greetingText() }
-         
             <input type="text" placeholder="Введите Ваше имя" onBlur ={(event) => this.handleChange(event)}></input>
           </div>
-      
+          </div>
     );
   }
 }
